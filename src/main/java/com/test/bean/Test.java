f2ef6a5556bb1ecbebc3bb3066dd8011a9f9568b
孙中军 <shulongszj@163.com>
2018-10-22 10:01:37 +0800

    在测试类中添加属性

--- src/main/java/com/test/bean/Test.java
+++ src/main/java/com/test/bean/Test.java
@@ -7,4 +7,17 @@
 package com.test.bean;
 
 public class Test {
+    private String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
